1fdeacf3ebf5e0ca379366cf325dde5f62f88748
Zakaria-Tafesh <zakaria.tafesh@gmail.com>
2021-11-07 18:57:32 +0200

    Initial commit

---
+++ run_server.sh
@@ -0,0 +1,18 @@
+
+cd /home/ubuntu/my_project/bin/ 
+source activate
+cd ..
+python3 manage.py runserver 0:8000 &
+
+
+
+
+
+
+####################################
+#!/bin/bash
+source /home/ubuntu/.bashrc
+source /home/ubuntu/my_project/bin/activate
+python /home/ubuntu/my_project/manage.py runserver 0:8000
+
+
