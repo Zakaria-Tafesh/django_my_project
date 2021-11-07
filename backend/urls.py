24095f39f143d07ba698a6ba38f324dcf98c8431
Zakaria-Tafesh <zakaria.tafesh@gmail.com>
2021-11-07 20:33:28 +0200

    test

--- backend/urls.py
+++ backend/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('register/', RegistrationAPI.as_view()),
     path('login/', LoginAPI.as_view()),
     path('user/', UserAPI.as_view()),
+
+    path('logout/', knox_views.LogoutView.as_view(), name='logout'),
+    path('logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
+
 ]
